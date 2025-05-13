2ec5bf9798f1e9b59409aa07e209eab2ea15e498
Cristhian <xcrissx12345@gmail.com>
2025-05-13 17:36:50 -0500

    Configcorrcatal4

--- models/catalogo.py
+++ models/catalogo.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 class OBJCatalogo(BaseModel):
     id: int
-    img: str    # aquí irá la ruta de la imagen
+    imagen: str    # aquí irá la ruta de la imagen
     nombre: str  
     fecha: str
     descripcion: str      

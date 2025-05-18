c13203ee7a9fc5f74793728b6d36ed8238d1678e
Cristhian <70170324+CRISTHIAN390@users.noreply.github.com>
2025-05-18 12:47:33 -0500

    ConfigInfofin

--- cruds/informacion.py
+++ cruds/informacion.py
@@ -24,8 +24,8 @@ def obtener_informacion(categoria: str):
                 de hacerlo si se entrega algún territorio como parte de un acuerdo de 
                 paz es simplemente estúpido. 
                 </p>
-                <div class="image-container">
-                    <img src="https://backend-zarel.onrender.com/static/img/tele.png" alt="Imagen relacionada" class="image-content" />
+                <div class="detalle-noticia-image-container-123">
+                    <img src="https://backend-zarel.onrender.com/static/img/tele.png" alt="Imagen relacionada" class="detalle-noticia-image-content-123" />
                 </div> 
                <p>
                 Biden habló justo cuando se cumplió el 

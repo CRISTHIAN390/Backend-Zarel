9c4fec3823f30d350e05a6fce3b4eeb3dbead9ce
Cristhian <70170324+CRISTHIAN390@users.noreply.github.com>
2025-05-18 11:09:35 -0500

    ConfigData

--- cruds/informacion.py
+++ cruds/informacion.py
@@ -11,7 +11,25 @@ def obtener_informacion(categoria: str):
                 url=base_url + "tele.png",
                 fecha="2025-05-07",
                 hora="05:30",
-                descripcion="Durante la conversacion, que ocurrio en Delaware el lunes, Biden afirmó que el mandatario ruso, Vladimir Putin, considera que Ucrania es parte de su país, y que cualquiera que piense que va a dejar de hacerlo si se entrega algún territorio como parte de un acuerdo de paz es simplemente estúpido. Biden habló justo cuando se cumplió el 80 aniversario del Día de la Victoria, que conmemora el fin de la Segunda Guerra Mundial, y aseguró que le preocupaba que las relaciones entre Estados Unidos y Europa se estén rompiendo bajo la presidencia de Donald Trump, lo que cambiaría la historia moderna del mundo",
+                #descripcion="Durante la conversacion, que ocurrio en Delaware el lunes, Biden afirmó que el mandatario ruso, Vladimir Putin, considera que Ucrania es parte de su país, y que cualquiera que piense que va a dejar de hacerlo si se entrega algún territorio como parte de un acuerdo de paz es simplemente estúpido. Biden habló justo cuando se cumplió el 80 aniversario del Día de la Victoria, que conmemora el fin de la Segunda Guerra Mundial, y aseguró que le preocupaba que las relaciones entre Estados Unidos y Europa se estén rompiendo bajo la presidencia de Donald Trump, lo que cambiaría la historia moderna del mundo",
+                #descripcion = "Esto es <strong>texto en negrita</strong> y esto es <em>cursiva</em>."
+                descripcion = """
+                <p>Este es el primer párrafo con <strong>algo en negrita</strong>.</p>
+                <br>
+                <p>Este es el segundo párrafo con una palabra en <em>cursiva</em>.</p>
+                <br>
+                <p>
+                Durante la conversacion, que ocurrio en Delaware el lunes, Biden afirmó
+                que el mandatario ruso, Vladimir Putin, considera que Ucrania
+                es parte de su país, y que cualquiera que piense que va a dejar
+                de hacerlo si se entrega algún territorio como parte de un acuerdo de 
+                paz es simplemente estúpido. Biden habló justo cuando se cumplió el 
+                80 aniversario del Día de la Victoria, que conmemora el fin de la 
+                Segunda Guerra Mundial, y aseguró que le preocupaba que las 
+                relaciones entre Estados Unidos y Europa se estén rompiendo 
+                bajo la presidencia de Donald Trump, lo que cambiaría la historia moderna del mundo
+                </p>
+                """.strip(),
                 estado=True
             ),
             OBJCategoria(

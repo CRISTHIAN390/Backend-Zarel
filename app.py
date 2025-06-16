4418d5dd2e7f0a61a1733e5d7cc420c56fbb8790
Cristhian <70170324+CRISTHIAN390@users.noreply.github.com>
2025-06-16 17:13:38 -0500

    ConfigListo_video

--- app.py
+++ app.py
@@ -69,7 +69,8 @@ def get_stream_url():
     # Aquí puedes cambiar la URL dinámicamente si quieres  #?autoplay=1&mute=0
     #https://www.youtube.com/embed/Y-IlMeCCtIg?autoplay=1&mute=0
     #https://www.youtube.com/embed/7b3GhFqWPsc?autoplay=1&mute=0
-    stream_url = 'https://iframe.dacast.com/live/c2386b04-15aa-974b-6912-f8fd63cd782a/94e4c98b-d4d3-8584-2d0c-3338128283ba'
+    #https://iframe.dacast.com/live/c2386b04-15aa-974b-6912-f8fd63cd782a/94e4c98b-d4d3-8584-2d0c-3338128283ba
+    stream_url = 'https://player.twitch.tv/?channel=lumin778&parent=backend-zarel.onrender.com'
     return jsonify({'stream_url': stream_url})
 
 

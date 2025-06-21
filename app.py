178a4990f6c762360d986ccf3b040490cf453d01
Cristhian <70170324+CRISTHIAN390@users.noreply.github.com>
2025-06-21 11:26:24 -0500

    ConfigTV2

--- app.py
+++ app.py
@@ -69,11 +69,14 @@ def get_stream_url():
     # Aquí puedes cambiar la URL dinámicamente si quieres  #?autoplay=1&mute=0
     #https://www.youtube.com/embed/Y-IlMeCCtIg?autoplay=1&mute=0
     #https://www.youtube.com/embed/7b3GhFqWPsc?autoplay=1&mute=0
+    #https://www.youtube.com/embed/Nkrl3cfaqKg?autoplay=1&mute=0
     #https://iframe.dacast.com/live/c2386b04-15aa-974b-6912-f8fd63cd782a/94e4c98b-d4d3-8584-2d0c-3338128283ba
     #https://player.twitch.tv/?channel=lumin778&parent=localhost
-    stream_url = 'https://player.twitch.tv/?channel=lumin778&parent=localhost'
+    #stream_url = 'https://player.twitch.tv/?channel=lumin778&parent=localhost'
+    #return jsonify({'stream_url': stream_url})
+    parent = request.args.get('parent', 'localhost')
+    stream_url = f'https://player.twitch.tv/?channel=lumin778&parent={parent}'
     return jsonify({'stream_url': stream_url})
-
 
 # Tu endpoint POST
 @app.route('/api/informacion', methods=['POST'])
